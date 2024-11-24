595f3f3dd6d97cb22cff984e776c358d289df1a9
miguelrosasvillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-11-24 18:17:33 -0300

    version 2

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,6 @@ alias lst='exa --icons --tree --level=2'
 alias cat='bat'
 alias matlab='/usr/local/MATLAB/R2024a/bin/matlab'
 
+
 # Created by `pipx` on 2024-03-17 19:22:51
 set PATH $PATH /home/miguel/.local/bin
